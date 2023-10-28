39b594d8c1b1138aca86e366eaa8d753ab4264a1
Fugazzii <sichinavailia@gmail.com>
2023-10-29 00:06:12 +0400

    Removed orchestrator-lib & Created LICENSE

--- libs/orchestrator-lib/src/orchestrator-lib.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OrchestratorLibService } from './orchestrator-lib.service';
-
-@Module({
-  providers: [OrchestratorLibService],
-  exports: [OrchestratorLibService],
-})
-export class OrchestratorLibModule {}
