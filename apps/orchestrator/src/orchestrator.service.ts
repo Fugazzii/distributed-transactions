c9adaac88485dc9597d2ecaf1e14425ccaad8d39
Fugazzii <sichinavailia@gmail.com>
2023-11-09 10:13:36 +0400

    Made code more readable

--- apps/orchestrator/src/orchestrator.service.ts
+++ apps/orchestrator/src/orchestrator.service.ts
@@ -2,7 +2,7 @@ import { AccountResponse, CreateAccountDto } from '@app/accounts-lib';
 import { ITransaction } from '@app/common';
 import { AccountEvent, AccountMessage, BlacklistMessage, Queue, TransactionEvent, TransactionMessage } from '@app/rmq';
 import { NewTxDto } from '@app/transactions-lib';
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { lastValueFrom } from 'rxjs';
 
@@ -27,47 +27,32 @@ export class OrchestratorService {
     const { password, ...txDetails } = newTxDto;
     const txDetailsString = JSON.stringify(txDetails);
 
-    // Publish verify message to accounts ms
-    const accountResponse$ = this.accountsQueue.send<ITransaction>(
-      AccountMessage.VERIFY, 
-      txDetailsString
-    );
-    
-    let accountTransaction: ITransaction = await lastValueFrom(accountResponse$);    
-    if(!accountTransaction) {
-      return this.failureResponse("Transaction failed due to insufficient funds");
+    // Handle the account verification
+    const accountTransaction = await this.handleAccount(txDetailsString);
+    if (!accountTransaction) {
+        return this.failureResponse("Transaction failed due to insufficient funds");
     }
 
     // Destructure transaction members
     const { amount, ...txMembers } = txDetails;
     const txMembersString = JSON.stringify(txMembers);
 
-    // Publish verify message to blacklist ms
-    const blacklistResponse$ = this.blacklistQueue.send<boolean>(
-      BlacklistMessage.VERIFY,
-      txMembersString
-    );
-
-    let blacklistMsSuccessed: boolean = await lastValueFrom(blacklistResponse$);
-    if(!blacklistMsSuccessed) {
-      return this.failureResponse("Transaction failed due to blacklisted member");
+    // Handle the blacklist verification
+    const blacklistMsSuccessed = await this.handleBlacklist(txMembersString);
+    if (!blacklistMsSuccessed) {
+        return this.failureResponse("Transaction failed due to blacklisted member");
     }
 
-    // Publish verify message to transactions ms
-    const transactionResponse$ = this.transactionsQueue.send<ITransaction>(
-      TransactionMessage.ADD,
-      txDetailsString
-    );
-
-    let txTransaction: ITransaction = await lastValueFrom(transactionResponse$);
-    if(!txTransaction) {
-      //! Rollback all transactions if here fails
-      this.rollbackAccounts(accountTransaction);
-      this.rollbackTransactions(txTransaction)
-      return this.failureResponse("Failed to perform transaction");
+    // Handle the transaction addition
+    const txTransaction = await this.handleTxs(txDetailsString);
+    if (!txTransaction) {
+        // Rollback all transactions if this fails
+        this.rollbackAccounts(accountTransaction);
+        this.rollbackTransactions(txTransaction);
+        return this.failureResponse("Failed to perform transaction");
     }
-    
-    //* Commit all transactions in case of success
+
+    // Commit all transactions in case of success
     this.commitAccounts(accountTransaction);
     this.commitTransactions(txTransaction);
 
@@ -91,6 +76,37 @@ export class OrchestratorService {
     return this.successResponse<AccountResponse>("Account successfully created", account);
   }
 
+  /** 
+   * Microservice handlers
+   */
+
+  private async handleAccount(txDetailsString: string): Promise<ITransaction | null> {
+    const accountResponse$ = this.accountsQueue.send<ITransaction>(
+        AccountMessage.VERIFY,
+        txDetailsString
+    );
+
+    return await lastValueFrom(accountResponse$);
+  }
+
+  private async handleBlacklist(txMembersString: string): Promise<boolean> {
+    const blacklistResponse$ = this.blacklistQueue.send<boolean>(
+        BlacklistMessage.VERIFY,
+        txMembersString
+    );
+
+    return await lastValueFrom(blacklistResponse$);
+  }
+
+  private async handleTxs(txDetailsString: string): Promise<ITransaction | null> {
+    const transactionResponse$ = this.transactionsQueue.send<ITransaction>(
+        TransactionMessage.ADD,
+        txDetailsString
+    );
+
+    return await lastValueFrom(transactionResponse$);
+  }
+  
   /**
    * RESPONSES
    */
